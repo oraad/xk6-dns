858ae62605f2b690c2541251235a9ff9f56c4102
oleiade <theo@crevon.me>
2024-06-12 15:55:52 +0200

    Implement underlying dns client

---
+++ dns/nameserver.go
@@ -0,0 +1,85 @@
+package dns
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+// Nameserver represents a DNS nameserver.
+type Nameserver struct {
+	// IPAddr is the IP address of the nameserver.
+	IP net.IP
+
+	// Port is the port of the nameserver.
+	Port uint16
+}
+
+// Addr returns the address of the nameserver as a string.
+func (n Nameserver) Addr() string {
+	return n.IP.String() + ":" + strconv.Itoa(int(n.Port))
+}
+
+// ParseNameserverAddr parses a nameserver address string into an IP and a port.
+//
+// It expects the `addr` to be in the format `ip` or `ip[:port]`. Where `ip` can be an IPv4 or an IPv6 address.
+func ParseNameserverAddr(addr string) (Nameserver, error) {
+	hostStr, portStr, err := parseHostAndPort(addr)
+	if err != nil {
+		return Nameserver{}, err
+	}
+
+	// Parse the host part into an IP
+	ip := net.ParseIP(hostStr)
+	if ip == nil {
+		return Nameserver{}, fmt.Errorf("invalid nameserver IP address: %s", hostStr)
+	}
+
+	if portStr == "" {
+		portStr = "53"
+	}
+
+	// Convert the port to an integer
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return Nameserver{}, fmt.Errorf("nameserver port is not a number: %w", err)
+	}
+
+	if port < 0 || port > 65535 {
+		return Nameserver{}, fmt.Errorf("nameserver port out of range: %d", port)
+	}
+
+	return Nameserver{ip, uint16(port)}, nil
+}
+
+func parseHostAndPort(addr string) (string, string, error) {
+	var err error
+	var host string
+	port := "53"
+
+	// Check if the address contains a port
+	if strings.ContainsRune(addr, ':') {
+		// Split the host and the port from the address string
+		host, port, err = net.SplitHostPort(addr)
+		if err == nil {
+			return host, port, nil
+		}
+
+		// IPv6 addresses can contain colons, so we need to check if the error is due to an IPv6 address
+		// without a port, or if it's an actual error.
+		if strings.Contains(addr, "]") {
+			// Try to trim the brackets from the IPv6 address and parse it without the port
+			if addr[0] == '[' && addr[len(addr)-1] == ']' {
+				host = addr[1 : len(addr)-1]
+				return host, port, nil
+			}
+		}
+
+		return "", "", fmt.Errorf("invalid nameserver address format: %w", err)
+	}
+
+	host = addr
+
+	return host, port, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,23 @@
 module github.com/grafana/xk6-dns
 
-go 1.20
+go 1.21
+
+toolchain go1.22.3
+
+require (
+	github.com/miekg/dns v1.1.59
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
